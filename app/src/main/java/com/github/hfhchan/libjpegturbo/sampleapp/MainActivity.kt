4c17b35f548d15a092bee1df7525b6b2365c69f9
Henry Chan <hfhchan@users.noreply.github.com>
2021-06-08 16:12:24 +0800

    Add sample code using TJCompressor

--- app/src/main/java/com/github/hfhchan/libjpegturbo/sampleapp/MainActivity.kt
+++ app/src/main/java/com/github/hfhchan/libjpegturbo/sampleapp/MainActivity.kt
@@ -4,6 +4,10 @@ import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.github.hfhchan.libjpegturbo.sampleapp.databinding.ActivityMainBinding
+import org.libjpegturbo.turbojpeg.TJ
+import org.libjpegturbo.turbojpeg.TJCompressor
+import java.nio.ByteBuffer
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +21,22 @@ class MainActivity : AppCompatActivity() {
 
         try {
             val byteFile = assets.open("negative-space-sandpiper_rock_nature_beach.jpg").readBytes()
-            val output = BitmapFactory.decodeByteArray(byteFile, 0, byteFile.size)
+            val bmp = BitmapFactory.decodeByteArray(byteFile, 0, byteFile.size)
+
+            val size: Int = bmp.rowBytes * bmp.height
+            val bytes = ByteArray(size)
+            val b: ByteBuffer = ByteBuffer.wrap(bytes)
+            bmp.copyPixelsToBuffer(b)
+            bmp.recycle()
+
+            val tjc = TJCompressor()
+            tjc.setSourceImage(bytes, 0, 0, bmp.width, 0, bmp.height, TJ.PF_RGBX)
+            tjc.setSubsamp(TJ.SAMP_444)
+            tjc.setJPEGQuality(100)
+            val outputBytes: ByteArray = tjc.compress(0)
+
+            val output = BitmapFactory.decodeByteArray(outputBytes, 0, tjc.compressedSize)
+            tjc.close()
 
             binding.sampleImg.setImageBitmap(output)
             binding.sampleText.text =
